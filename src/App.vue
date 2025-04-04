07823a4416246bcebe3f994d0a848283b4b97566
John Knotts <jhnnyk@gmail.com>
2025-04-04 16:04:31 -0600

    setting up a basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <NavBar />
+    <RouterView />
+  </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a href="/" class="navbar-item"> sk8prks.com </a>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu">
+      <RouterLink class="navbar-item" to="/">Home</RouterLink>
+      <RouterLink class="navbar-item" to="/about">About</RouterLink>
+    </div>
+  </nav>
+</template>
