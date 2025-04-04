30457dc9154e97cf24c3e1cf064ef51757706fa2
John Knotts <jhnnyk@gmail.com>
2025-04-04 17:24:07 -0600

    display skatepark from DB

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase'
+import { ref, onMounted } from 'vue'
+
+const skateparks = ref([])
+
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, 'skateparks'))
+  let dbSkateparks = []
+  querySnapshot.forEach((doc) => {
+    const park = {
+      id: doc.id,
+      title: doc.data().title,
+      description: doc.data().description,
+    }
+    dbSkateparks.push(park)
+  })
+  skateparks.value = dbSkateparks
+})
+</script>
 
 <template>
   <main>hello world</main>
+  <ul v-for="park in skateparks">
+    <li>{{ park.title }}</li>
+  </ul>
 </template>
