b00fbeaa44016e4cda74c210025210492503ce39
John Knotts <jhnnyk@gmail.com>
2025-04-04 16:14:21 -0600

    mobile nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { ref } from 'vue'
+const showMobileNav = ref(false)
+</script>
+
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
@@ -9,6 +14,8 @@
         aria-label="menu"
         aria-expanded="false"
         data-target="navbarBasicExample"
+        @click="showMobileNav = !showMobileNav"
+        :class="{ 'is-active': showMobileNav }"
       >
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -17,9 +24,9 @@
       </a>
     </div>
 
-    <div class="navbar-menu">
-      <RouterLink class="navbar-item" to="/">Home</RouterLink>
-      <RouterLink class="navbar-item" to="/about">About</RouterLink>
+    <div class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+      <RouterLink class="navbar-item" @click="showMobileNav = false" to="/">Home</RouterLink>
+      <RouterLink class="navbar-item" @click="showMobileNav = false" to="/about">About</RouterLink>
     </div>
   </nav>
 </template>
