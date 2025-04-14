4861075f110ae2cab72ffc53b5d398e64c214935
John Knotts <jhnnyk@gmail.com>
2025-04-14 13:40:23 -0600

    user logout

--- src/views/admin/LoginView.vue
+++ src/views/admin/LoginView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { useUserStore } from '@/stores/UserStore'
 
 const auth = getAuth()
@@ -23,10 +23,22 @@ const login = () => {
       console.log(error.code, error.message)
     })
 }
+
+const logout = () => {
+  signOut(auth)
+    .then(() => {
+      userStore.clearUser()
+      console.log('user signed out')
+    })
+    .catch((error) => {
+      console.log('Error signing out: ', error)
+    })
+}
 </script>
 
 <template>
-  <section class="section">
+  <section class="section" v-if="!userStore.user">
+    <h1 class="is-size-1 has-text-centered">Login</h1>
     <form @submit.prevent="login">
       <div class="field">
         <label class="label">Email</label>
@@ -42,5 +54,13 @@ const login = () => {
       </div>
       <button class="button is-primary">Sign In</button>
     </form>
+  </section>
+
+  <section class="section" v-else>
+    <h2 class="is-size-2 has-text-centered">Already logged in as:</h2>
+    <p class="has-text-centered">{{ userStore.user.email }}</p>
+    <p class="has-text-centered my-5">
+      <button @click="logout" class="button is-link">Log out</button>
+    </p>
   </section>
 </template>
