2057fdaec8d60410bead147c7c645bcbb829634c
John Knotts <jhnnyk@gmail.com>
2025-05-05 12:26:58 -0600

    display skatepark count

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,9 @@ const userStore = useUserStore()
 </script>
 
 <template>
-  <section class="section">
-    <h1 class="title">Welcome to sk8prks.com!</h1>
+  <section class="section has-text-centered">
+    <h1 class="title mb-0">Welcome to sk8prks.com</h1>
+    <p class="mb-5">{{ skateparkStore.getParks.length }} skateparks and counting!</p>
 
     <ul class="grid is-col-min-10">
       <li v-for="park in skateparkStore.getParks" :key="park.id" class="cell has-text-centered">
