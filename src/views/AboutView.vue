9fe00d343006e19aca4fdc0c3e11ae826628f238
John Knotts <jhnnyk@gmail.com>
2025-05-10 16:07:26 -0600

    add title

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="section">
+    <h1 class="is-size-1">About Us</h1>
     <p>
       Started in February 2025, this is just a little project to get me out of my routine and see
       how many different skateparks I can visit.
