e4f23e85362917f43be9f6342c540763bd072035
John Knotts <jhnnyk@gmail.com>
2025-05-10 16:11:31 -0600

    add header image

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="section">
+    <img src="/images/site/westlake_00_header.jpg" alt="Westlake Skatepark at sunset" />
     <h1 class="is-size-1">About Us</h1>
     <p>
       Started in February 2025, this is just a little project to get me out of my routine and see
