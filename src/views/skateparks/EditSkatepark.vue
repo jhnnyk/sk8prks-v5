62a3d3a24007d43cadf463675ad43e594b7c6aeb
John Knotts <jhnnyk@gmail.com>
2025-05-15 12:58:36 -0600

    ability to set date

--- src/views/skateparks/EditSkatepark.vue
+++ src/views/skateparks/EditSkatepark.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useSkateparkStore } from '@/stores/SkateparkStore'
-import { onUpdated } from 'vue'
+import { onMounted, onUpdated, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { doc, setDoc } from 'firebase/firestore'
 import { db } from '@/firebase'
@@ -10,6 +10,16 @@ import EditImages from '@/components/skatepark-admin/EditImages.vue'
 const skateparkStore = useSkateparkStore()
 const router = useRouter()
 
+const year = ref('')
+const month = ref('')
+const day = ref('')
+
+onMounted(() => {
+  year.value = skateparkStore.getCurrentPark.lastUpdated.substring(0, 4)
+  month.value = skateparkStore.getCurrentPark.lastUpdated.substring(4, 6)
+  day.value = skateparkStore.getCurrentPark.lastUpdated.substring(6, 8)
+})
+
 onUpdated(() => {
   skateparkStore.getCurrentPark
 })
@@ -17,6 +27,7 @@ onUpdated(() => {
 const updatePark = async () => {
   // save to DB
   await setDoc(doc(db, 'skateparks', skateparkStore.getCurrentPark.id), {
+    lastUpdated: year.value + month.value + day.value,
     title: skateparkStore.getCurrentPark.title,
     slug: skateparkStore.getCurrentPark.slug,
     street: skateparkStore.getCurrentPark.street,
@@ -47,6 +58,70 @@ const updatePark = async () => {
   <section v-else class="section">
     <form @submit.prevent="updatePark">
       <h1 class="title">Edit skatepark</h1>
+
+      <div class="field">
+        <label class="label">Last updated:</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="year" required>
+              <option value="2025">2025</option>
+            </select>
+          </div>
+
+          <div class="select">
+            <select v-model="month" required>
+              <option value="01">01</option>
+              <option value="02">02</option>
+              <option value="03">03</option>
+              <option value="04">04</option>
+              <option value="05">05</option>
+              <option value="06">06</option>
+              <option value="07">07</option>
+              <option value="08">08</option>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+          </div>
+
+          <div class="select">
+            <select v-model="day" required>
+              <option value="01">01</option>
+              <option value="02">02</option>
+              <option value="03">03</option>
+              <option value="04">04</option>
+              <option value="05">05</option>
+              <option value="06">06</option>
+              <option value="07">07</option>
+              <option value="08">08</option>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+              <option value="27">27</option>
+              <option value="28">28</option>
+              <option value="29">29</option>
+              <option value="30">30</option>
+              <option value="31">31</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
       <div class="field">
         <label class="label">Title</label>
