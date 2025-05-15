f4aac1d3839a4e079ea25a936df2320b611fc10b
John Knotts <jhnnyk@gmail.com>
2025-05-15 15:58:29 -0600

    styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,23 +23,19 @@ const months = [
   <section class="section has-text-centered">
     <h1 class="is-size-1 mb-2">Welcome to sk8prks.com</h1>
 
-    <h2 class="is-size-3 is-uppercase">Recently Added / Updated</h2>
+    <h2 class="is-size-3 is-uppercase has-text-left">Recently Added / Updated</h2>
 
-    <ul class="grid is-col-min-10">
-      <li
-        v-for="park in skateparkStore.getRecentlyUpdatedParks"
-        :key="park.id"
-        class="cell has-text-centered"
-      >
+    <ul class="grid is-col-min-10 is-row-gap-3">
+      <li v-for="park in skateparkStore.getRecentlyUpdatedParks" :key="park.id" class="cell">
         <RouterLink :to="`/skateparks/${park.state.slice(3)}/${park.slug}`">
           <img :src="park.images[1].path" :alt="park.images[1].alt_text" />
 
-          {{ park.title }}<br />
-          <span class="is-size-7">
+          <p>{{ park.title }}</p>
+          <p class="is-size-7">
             Updated: {{ months[park.lastUpdated.substring(4, 6) - 1] }}
             {{ park.lastUpdated.substring(6, 8) }},
             {{ park.lastUpdated.substring(0, 4) }}
-          </span>
+          </p>
         </RouterLink>
       </li>
     </ul>
